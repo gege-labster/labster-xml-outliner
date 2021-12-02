3393fa93869ec02aae5b42623605256f435b93b9
gege <gakbar@labster.com>
2021-12-02 13:29:03 +0700

    fix helloworld bug

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,6 @@ import { XMLOutlineProvider } from './xmlprocessor';
 export function activate(context: vscode.ExtensionContext) {
 
 	const xmlOutlineProvider = new XMLOutlineProvider(context);
-
-	console.log('Congratulations, your extension "labster-xml-outliner" is now active!');
-	context.subscriptions.push(vscode.commands.registerCommand('labster-xml-outliner.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello, beautiful developers!!! Hope you have a good day.');
-	}));
 	context.subscriptions.push(vscode.window.registerTreeDataProvider('labsterXMLOutline', xmlOutlineProvider));
 	context.subscriptions.push(vscode.commands.registerCommand('labster-xml-outliner.refresh', () => {
 		xmlOutlineProvider.refresh();
