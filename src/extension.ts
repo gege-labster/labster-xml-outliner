e414a355087839883b10ca297777b2a91f130ffb
gege <gakbar@labster.com>
2021-12-02 12:57:00 +0700

    complete first version

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { XMLOutlineProvider } from './xmlprocessor';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+
+	const xmlOutlineProvider = new XMLOutlineProvider(context);
+
 	console.log('Congratulations, your extension "labster-xml-outliner" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('labster-xml-outliner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Labster XML Outliner!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('labster-xml-outliner.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello, beautiful developers!!! Hope you have a good day.');
+	}));
+	context.subscriptions.push(vscode.window.registerTreeDataProvider('labsterXMLOutline', xmlOutlineProvider));
+	context.subscriptions.push(vscode.commands.registerCommand('labster-xml-outliner.refresh', () => {
+		xmlOutlineProvider.refresh();
+	}));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
---
+++ src/xmlProcessor.ts
@@ -0,0 +1,150 @@
+import * as vscode from "vscode";
+const xml2js = require("xml2js");
+import * as XMLParser from "fast-xml-parser";
+
+export class XMLOutlineProvider implements vscode.TreeDataProvider<EngineXML>{
+  public engine: EngineXML;
+
+  private _onDidChangeTreeData: vscode.EventEmitter<EngineXML | undefined | null | void> = new vscode.EventEmitter<EngineXML | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<EngineXML | undefined | null | void> = this._onDidChangeTreeData.event;
+
+  refresh(): void {
+    this.engine = this.parseXML();
+    this._onDidChangeTreeData.fire();
+  }
+
+
+  constructor(private context: vscode.ExtensionContext) {
+    this.engine = this.parseXML();
+    vscode.window.onDidChangeActiveTextEditor(e => this.onActiveEditorChanged(e));
+    vscode.workspace.onDidChangeTextDocument(e => this.onTextChanged(e));
+    // console.log(this.engine);
+    // console.log(obj[0]);
+    this.onActiveEditorChanged(vscode.window.activeTextEditor);
+  }
+
+  getTreeItem(element: EngineXML): vscode.TreeItem {
+    vscode.window.showInformationMessage('shits happen on get Tree item');
+    return element;
+  }
+
+  getChildren(element?: EngineXML): Thenable<EngineXML[]> {
+    vscode.window.showInformationMessage('shits happen on get Children');
+    if (element) {
+      if (element.child) {
+        return Promise.resolve(element.child);
+      }
+    } else {
+
+      let buffer: EngineXML[] = [];
+      buffer.push(this.engine);
+      return Promise.resolve(buffer);
+    }
+    return Promise.resolve([]);
+  }
+
+
+
+  parseXML(): EngineXML {
+    let text: string;
+    if (vscode.window.activeTextEditor) {
+      text = vscode.window.activeTextEditor?.document.getText();
+    } else {
+      text = "";
+    }
+
+    const option = {
+      ignoreAttributes: false,
+      allowBooleanAttributes: true,
+      preserveOrder: true,
+      attributeNamePrefix: "",
+      isArray: (name: string, jpath: string, isLeafNode: boolean, isAttribute: boolean) => {
+        if (isAttribute || isLeafNode) {
+          return false;
+        }
+        return true;
+      }
+    };
+    const parser = new XMLParser.XMLParser(option);
+    let obj = parser.parse(text)[0];
+    return new EngineXML(obj);
+  }
+
+  onActiveEditorChanged(editor?: vscode.TextEditor) {
+    if (editor) {
+      if (editor.document.languageId === "xml") {
+        // console.log("activate view");
+        this.activateView();
+        this.refresh();
+      } else {
+        this.deactivateView();
+      }
+    }
+  }
+  onTextChanged(event: vscode.TextDocumentChangeEvent) {
+    if (event.document === vscode.window.activeTextEditor?.document) {
+      if (event.document.languageId === "xml") {
+        this.refresh();
+      }
+    }
+  }
+
+
+  static getXMLTagName(element: object): string {
+    let keys = Object.keys(element);
+    return keys[0];
+  }
+
+  static getXMLId(element: any): string | undefined {
+    if (element.attributes === undefined) { return; }
+    return element.attributes.Id;
+  }
+
+  static getXMLChild(element: any, tagName?: string): object {
+    if (tagName !== undefined) {
+      return element[tagName];
+    }
+    return element[this.getXMLTagName(element)];
+  }
+
+  activateView() {
+    vscode.commands.executeCommand("setContext", "fileIsXML", true);
+
+  }
+  deactivateView() {
+    vscode.commands.executeCommand("setContext", "fileIsXML", false);
+  }
+
+}
+
+
+
+
+class EngineXML extends vscode.TreeItem {
+  public child?: EngineXML[];
+  public tagName: string;
+  public xmlId: string | undefined = undefined;
+  constructor(
+    private element: any,
+  ) {
+    super(XMLOutlineProvider.getXMLTagName(element));
+    this.tagName = XMLOutlineProvider.getXMLTagName(this.element);
+    if (XMLOutlineProvider.getXMLId(element) !== undefined) {
+      this.xmlId = XMLOutlineProvider.getXMLId(this.element);
+      this.tooltip = `${this.tagName} - ${XMLOutlineProvider.getXMLId(this.element)}`;
+      this.description = this.xmlId;
+    } else {
+      this.tooltip = `${this.tagName}`;
+    }
+    if (Array.isArray(this.element[this.tagName]) && this.element[this.tagName].length > 0) {
+      let buffer: EngineXML[] = [];
+      this.element[this.tagName].forEach(function (element: object) {
+        buffer.push(new EngineXML(element));
+      });
+      this.child = buffer;
+      this.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
+    }
+
+  }
+}
+
